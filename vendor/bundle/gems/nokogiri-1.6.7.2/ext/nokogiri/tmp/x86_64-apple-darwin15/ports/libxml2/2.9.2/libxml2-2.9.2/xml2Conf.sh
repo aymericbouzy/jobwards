6f7bf0defde061ac156760ed77aad379606c4d23
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2016-02-20 16:38:56 -0800

    created rails app

---
+++ vendor/bundle/gems/nokogiri-1.6.7.2/ext/nokogiri/tmp/x86_64-apple-darwin15/ports/libxml2/2.9.2/libxml2-2.9.2/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/aymericbouzy/Sites/jobwards/vendor/bundle/gems/nokogiri-1.6.7.2/ports/x86_64-apple-darwin15/libxml2/2.9.2/lib"
+XML2_LIBS="-lxml2 -lz -lpthread  -liconv -lm "
+XML2_INCLUDEDIR="-I/Users/aymericbouzy/Sites/jobwards/vendor/bundle/gems/nokogiri-1.6.7.2/ports/x86_64-apple-darwin15/libxml2/2.9.2/include/libxml2"
+MODULE_VERSION="xml2-2.9.2"
+
